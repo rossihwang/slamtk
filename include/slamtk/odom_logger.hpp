d60c5e5a2afc3e55823a2e1d8fe14a8cd1dfbc93
ruoxi.huang <ruoxi.huang@fitkits.com.cn>
2021-07-24 11:55:48 +0800

    Support dynamic parameters

--- include/slamtk/odom_logger.hpp
+++ include/slamtk/odom_logger.hpp
@@ -5,13 +5,14 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <rcl_interfaces/msg/parameter.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/create_timer_ros.h>
 
 namespace toolkits {
 
 class OdomLogger: public rclcpp::Node {
- private:
+ protected:
   std::thread main_thread_;
   std::atomic<bool> canceled_;
   std::string log_filename_;
@@ -20,9 +21,15 @@ class OdomLogger: public rclcpp::Node {
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
 
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr set_param_callback_;
+
  public:
   OdomLogger(const std::string& name, rclcpp::NodeOptions const& options);
   ~OdomLogger();
+
+ protected:
+  void create_parameter();
+  rcl_interfaces::msg::SetParametersResult set_parameter_handle(const std::vector<rclcpp::Parameter>& parameters);
 };
 
 }  // namespace toolkits
--- src/odom_logger.cpp
+++ src/odom_logger.cpp
@@ -5,17 +5,19 @@
 #include <boost/format.hpp>
 #include <Eigen/Geometry>
 
+#include <rcl_interfaces/msg/parameter.hpp>
+
 namespace toolkits {
 
 using namespace Eigen;
+using std::placeholders::_1;
 
 OdomLogger::OdomLogger(const std::string& name, rclcpp::NodeOptions const& options) 
   : Node(name, options),
-    canceled_(false),
-    log_filename_("estimated_odom.txt"),
-    base_frame_("base_footprint"),
-    global_frame_("map") {
+    canceled_(false) {
   
+  create_parameter();
+
   tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
   auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
     this->get_node_base_interface(),
@@ -75,6 +77,32 @@ OdomLogger::~OdomLogger() {
   if (main_thread_.joinable()) {
     main_thread_.join();
   }
+}
+
+void OdomLogger::create_parameter() {
+  log_filename_ = declare_parameter<std::string>("log_filename", "estimated_odom.txt");
+  base_frame_ = declare_parameter<std::string>("base_frame", "base_footprint");
+  global_frame_ = declare_parameter<std::string>("global_frame", "map");
+
+  set_param_callback_ = add_on_set_parameters_callback(std::bind(&OdomLogger::set_parameter_handle, this, _1));
+}
+
+rcl_interfaces::msg::SetParametersResult OdomLogger::set_parameter_handle(const std::vector<rclcpp::Parameter>& parameters) {
+  rcl_interfaces::msg::SetParametersResult result;
+  result.successful = true;
+
+  for (const rclcpp::Parameter &param: parameters) {
+    if (param.get_name() == "log_filename") {
+      log_filename_ = param.as_string();
+    } else if (param.get_name() == "base_frame") {
+      base_frame_ = param.as_string();
+    } else if (param.get_name() == "global_frame") {
+      global_frame_ = param.as_string();
+    } else {
+      result.successful = false;
+    }
+  }
+  return result;
 }
 
 }  // namespace toolkits
